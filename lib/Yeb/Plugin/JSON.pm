04ea63ba4fae8a31d33d470407f171bd711855fa
Torsten Raudssus <torsten@raudssus.de>
2014-03-24 23:12:16 +0000

    Import of GETTY/Yeb-Plugin-JSON-0.101 from CPAN.
    gitpan-cpan-distribution: Yeb-Plugin-JSON
    gitpan-cpan-version:      0.101
    gitpan-cpan-path:         GETTY/Yeb-Plugin-JSON-0.101.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/Yeb/Plugin/JSON.pm
+++ lib/Yeb/Plugin/JSON.pm
@@ -3,31 +3,93 @@ BEGIN {
   $Yeb::Plugin::JSON::AUTHORITY = 'cpan:GETTY';
 }
 # ABSTRACT: Yeb Plugin for JSON response
-$Yeb::Plugin::JSON::VERSION = '0.100';
+$Yeb::Plugin::JSON::VERSION = '0.101';
 use Moo;
 use JSON::MaybeXS;
 
 has app => ( is => 'ro', required => 1, weak_ref => 1 );
 
+my @json_attrs_untouched = qw(
+  ascii latin1 binary pretty indent space_before space_after
+  relaxed canonical filter_json_object filter_json_single_key_object
+  shrink max_depth max_size 
+);
+
+for (@json_attrs_untouched) {
+  has $_ => (
+    is => 'ro',
+    predicate => 'has_'.$_,
+  );
+}
+
+my @json_attrs_enabled = qw(
+  utf8 allow_nonref convert_blessed allow_blessed
+);
+
+for (@json_attrs_enabled) {
+  has $_ => (
+    is => 'ro',
+    default => sub { 1 },
+  );
+}
+
+my @json_attrs = ( @json_attrs_untouched, @json_attrs_enabled );
+
+has json => (
+  is => 'ro',
+  lazy => 1,
+  default => sub {
+    my ( $self ) = @_;
+    my $json = JSON::MaybeXS->new;
+    for my $attr (@json_attrs) {
+      my $has_attr = 'has_'.$attr;
+      if (!$self->can($has_attr) || $self->$has_attr) {
+        $json->$attr($self->$attr);
+      }
+    }
+    return $json;
+  },
+);
+
+has json_class => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { (ref $_[0]->json) },
+);
+
+has true => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->json_class->true },
+);
+
+has false => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->json_class->false },
+);
+
 sub get_vars {
-	my ( $self, $user_vars ) = @_;
-	my %stash = %{$self->app->cc->stash};
-	my %user = defined $user_vars ? %{$user_vars} : ();
-	return $self->app->merge_hashs(
-		$self->app->cc->export,
-		\%user
-	);
+  my ( $self, $user_vars ) = @_;
+  my %stash = %{$self->app->cc->stash};
+  my %user = defined $user_vars ? %{$user_vars} : ();
+  return $self->app->merge_hashs(
+    $self->app->cc->export,
+    \%user
+  );
 }
 
 sub BUILD {
-	my ( $self ) = @_;
-	$self->app->register_function('json',sub {
-		my $user_vars = shift;
-		my $vars = $self->get_vars($user_vars);
-		$self->app->cc->content_type('application/json');
-		$self->app->cc->body(to_json($vars,@_));
-		$self->app->cc->response;
-	});
+  my ( $self ) = @_;
+  $self->app->register_function('json',sub {
+    my $user_vars = shift;
+    my $vars = $self->get_vars($user_vars);
+    $self->app->cc->content_type('application/json');
+    $self->app->cc->body($self->json->encode($vars));
+    $self->app->cc->response;
+  });
+  $self->app->register_function('true',sub { $self->true });
+  $self->app->register_function('false',sub { $self->false });
 }
 
 1;
@@ -42,7 +104,7 @@ Yeb::Plugin::JSON - Yeb Plugin for JSON response
 
 =head1 VERSION
 
-version 0.100
+version 0.101
 
 =head1 SYNOPSIS
 
